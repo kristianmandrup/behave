e4d9a8b313af0ba74e951c98f26fcbe96427c2ec
Kristian Mandrup <kmandrup@gmail.com>
2011-06-16 08:57:14 +0200

    initial commit - extracted from troles

--- spec/behave_spec.rb
+++ spec/behave_spec.rb
@@ -1,7 +1,35 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+# This is to test how you can add behaviors to the set of global behaviors!describe Behave::Macros do
+# The Behave module acts as a repository for registering behaviors that can then be added to individual classes
 
-describe "Behave" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+require 'behave/spec_helper'
+
+describe Behave do
+  describe '#add_behavior' do
+    it 'should add a named behavior' do
+      Behave.add_behavior(:hello).should be_a(Behave::Decorator)
+    end
+
+    it 'should add a behavior with config class' do
+      Behave.add_behavior(:hello) do |behavior|
+        behavior.should be_a(Behave::Decorator)
+      end
+    end
   end
+
+  describe '#behaviors' do  
+    it 'should contain global behaviors' do
+      Behave.behaviors.should_not be_empty
+    end
+    
+    it 'should have hello' do
+      Behave.behaviors[:hello].should be_a(Behave::Decorator)
+    end
+  end
+
+  describe 'behavior' do  
+    it 'should have behavior' do
+      Behave.behaviors[:hello].config.should be_a(Behave::Config)
+    end
+  end  
 end
+
